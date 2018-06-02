5d6b23161be82785fb4177ea9a6a3fe5dcff5a31
Teddy Koomen <teddykoomen@Teddys-MBP.home>
2018-06-01 21:33:13 -0400

    remove comment

--- src/styledComponents/MarginRight.jsx
+++ src/styledComponents/MarginRight.jsx
@@ -4,8 +4,5 @@ import { space } from 'styled-system'
 const MarginRight = styled.div`
   ${space}
 `
-// const MarginRight = styled('div')`
-//   margin-right: 10px;
-// `
 
 export default MarginRight
